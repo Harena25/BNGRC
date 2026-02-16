3ab996c66530354daa8f691873b915eb21d5fb92
Harena25 <harenarivony@gmail.com>
2026-02-16 13:54:45 +0300

    feat :base de donne shema

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,63 @@
+CREATE DATABASE IF NOT EXIST bngrc;
+USE bngrc;
 
+CREATE TABLE IF NOT EXISTS article (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    libelle VARCHAR(255) NOT NULL,
+    cat_article VARCHAR(255) NOT NULL,
+    prix_unitaire DECIMAL(10, 2) NOT NULL,
+); 
+
+CREATE TABLE IF NOT EXISTS besoin (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    ville_id INT NOT NULL,
+    article_id INT NOT NULL,
+    quantite INT NOT NULL,
+    date_besoin DATE NOT NULL,
+    status_id INT NOT NULL,
+    FOREIGN KEY (article_id) REFERENCES article(id),
+    FOREIGN KEY (ville_id) REFERENCES ville(id),
+    FOREIGN KEY (status_id) REFERENCES status(id)
+);
+
+CREATE TABLE IF NOT EXISTS status (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    libelle VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ville (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    region_id INT NOT NULL,
+    libelle VARCHAR(255) NOT NULL,
+    FOREIGN KEY (region_id) REFERENCES region(id)
+);
+
+CREATE TABLE IF NOT EXISTS region (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    libelle VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS distribution (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    besoin_id INT NOT NULL,
+    quantite_distribuee INT NOT NULL,
+    date_distribution DATE NOT NULL,
+    article_id INT NOT NULL,
+    FOREIGN KEY (besoin_id) REFERENCES besoin(id),
+    FOREIGN KEY (article_id) REFERENCES article(id)
+);
+
+CREATE TABLE IF NOT EXISTS dons (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    article_id INT NOT NULL,
+    quantite_donnee INT NOT NULL,
+    date_don DATE NOT NULL,
+    FOREIGN KEY (article_id) REFERENCES article(id)
+);
+
+CREATE TABLE IF NOT EXISTS stock (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    article_id INT NOT NULL,
+    quantite_stock INT NOT NULL,
+    FOREIGN KEY (article_id) REFERENCES article(id)
+);
